6578f1071971c9706edbcf9a698969884f4e2448
unknown <chepurnojv@gmail.com>
2022-10-18 13:09:59 +0300

    changed image

--- pages/eom.js
+++ pages/eom.js
@@ -15,7 +15,11 @@ export const EOM = ({ employee }) => {
 				<div className={styles.employeeOfTheMonth}>
 					<h3>{employee.name}</h3>
 					<h6>{employee.position}</h6>
-					<img src={employee.image} />
+					<img
+						src={
+							"https://upload.wikimedia.org/wikipedia/ru/9/94/%D0%93%D0%B8%D0%B3%D0%B0%D1%87%D0%B0%D0%B4.jpg"
+						}
+					/>
 					<p>{employee.description}</p>
 				</div>
 			</div>
